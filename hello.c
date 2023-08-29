951e41b252ebe1fb04f1e2c567ba43a700e16f94
andrewebster <142914358+andrewebster@users.noreply.github.com>
2023-08-28 22:42:56 -0500

    added my major to the output

--- hello.c
+++ hello.c
@@ -1,1 +1,10 @@
-printf("Andrew Webster\n");
+#include <stdlib.h>
+#include <stdio.h>
+int main(int argc, char **argv) {
+
+    printf("Andrew Webster\n");
+    printf("My major is computer science\n");
+
+return 0;
+
+}
